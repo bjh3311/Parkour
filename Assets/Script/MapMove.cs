99a6f3f8993639dd32e5413dc2f5e966f4809941
bjh3311 <bjh3311@naver.com>
2022-03-28 23:53:38 +0900

    Map 이동

---
+++ Assets/Script/MapMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMove : MonoBehaviour
+{
+    public float mapSpeed=0f;
+
+    private void Update() 
+    {
+        //mapSpeed만큼 -x축으로 이동
+        transform.Translate(0,0,-mapSpeed*Time.deltaTime);//z축으로 이동
+    }
+}
