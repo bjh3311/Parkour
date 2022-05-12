4e5fe93381309818dbf3736e301a4e147e0910c4
bjh3311 <bjh3311@naver.com>
2022-05-13 04:09:11 +0900

    시작화면 초안 완성

--- Assets/Script/MainManager.cs
+++ Assets/Script/MainManager.cs
@@ -15,6 +15,7 @@ public class MainManager : MonoBehaviour
     private float threshold=1.01f;
     [SerializeField]
     private float dissolveTime;
+    public Text Loading;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,13 +23,14 @@ public class MainManager : MonoBehaviour
     }
     public void GameStart()
     {
+        Black.gameObject.SetActive(true);
         StartCoroutine("Move");
         player_animator.SetBool("isRun",true);
         foreach(Button temp in Buttons)
         {
             temp.interactable=false;
         }
-        StartCoroutine("Transparent");
+        StartCoroutine("Transparent"); 
     }
     public void Exit()
     {
@@ -46,8 +48,8 @@ public class MainManager : MonoBehaviour
             player_transform.position=player_transform.position+new Vector3(0,0,-2);
             if(player_transform.position.z<-20)
             {
-                break;
                 StartCoroutine("Mozaic");
+                break;
             }
             
         }
@@ -71,6 +73,7 @@ public class MainManager : MonoBehaviour
     }
     IEnumerator Mozaic()
     {
+        
          while(true)
         {
             Black.material.SetFloat("_Threshold", threshold);
@@ -78,8 +81,48 @@ public class MainManager : MonoBehaviour
             yield return new WaitForSeconds(dissolveTime);    
             if(threshold<=0.0f)
             {
+                yield return new WaitForSeconds(0.5f);
+                Loading.gameObject.SetActive(true);
+                StartCoroutine("TypingEffect","......");
+                StartCoroutine("Load");
                 break;
             }
+            
         }
     }
+    private IEnumerator TypingEffect(string dot)//Loading.... 에서 . 을 타이핑되는듯이 계속
+    {   
+        int i=0;
+        while(true)
+        {
+            for(i=0;i<dot.Length;i++)
+            {
+                Loading.text="Loading"+dot.Substring(0,i+1);
+                yield return new WaitForSeconds(0.5f);
+            }
+            Loading.text="Loading";
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+    private IEnumerator Load()//씬 불러오기
+    {
+        AsyncOperation op=SceneManager.LoadSceneAsync("Stage");
+        //비동기 방식으로 씬을 불러오는 도중에도 다른 작업을 할 수  LoadSceneAsync 함수
+        //로딩의 진행정도는 AsyncOperation Class로 반환된다
+        op.allowSceneActivation=false;//로딩이 끝나면 씬을 바로 시작 못하게 한다
+        float timer=0.0f;
+        while(!op.isDone)//isDone이 false일 때 동안, 즉 Load가 진행중을 의미한다
+        {
+            yield return null;
+            timer+=Time.unscaledDeltaTime;
+            if(timer>=1.0f)//타이머가 지나면
+            {
+                yield return new WaitForSeconds(3.5f);
+                op.allowSceneActivation=true;
+                break;
+            }
+        }    
+    }
+    
+
 }
