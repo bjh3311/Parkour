303914d47923b5bd5da75d6e1adcf252567c1939
bjh3311 <bjh3311@naver.com>
2022-08-26 02:30:40 +0900

    ppt 6쪽까지 진행함

---
+++ Assets/Script/Login/KakaoLogin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KakaoLogin : MonoBehaviour
+{
+    private AndroidJavaObject ajo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ajo=new AndroidJavaObject("com.Redtiger.Parkour3D.UKakao");
+    }
+    public void Login()//로그인 버튼
+    {
+        ajo.Call("KakaoLogin");//UKakao라는 코틀린파일에서 KakaoLogin이라는 함수를 호출한다
+    }
+}
