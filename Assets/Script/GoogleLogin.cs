d4837440685f65e801b15fb26fb71dbcc187f8d8
bjh3311 <bjh3311@naver.com>
2022-09-05 21:46:13 +0900

    audohfdaj

--- Assets/Script/GoogleLogin.cs
+++ Assets/Script/GoogleLogin.cs
@@ -11,6 +11,8 @@ using GooglePlayGames.BasicApi;
 
 public class GoogleLogin : MonoBehaviour
 {
+
+    
     public Text temp;
     void Start()
     {
@@ -21,25 +23,7 @@ public class GoogleLogin : MonoBehaviour
     }
     public void Login()//구글플레이 로그인 버튼에 적용
     {
-        if(!Social.localUser.authenticated)//현재 기기와 연결된 계정이 인증이 아직 안됐는가?
-        {
-            Social.localUser.Authenticate(success => 
-            {
-                if(success)//로그인에 성공시
-                {
-                    Debug.Log("Authentication successful");
-                    string userInfo="Username: " + Social.localUser.userName +
-                    "\nUser ID: " + Social.localUser.id +
-                    "\nIsUnderage: " + Social.localUser.underage;
-                    temp.text=userInfo;
-                    Debug.Log(userInfo);
-                }
-                else
-                {
-                    Debug.Log("Authentication Failed");
-                }
-            });
-        }
+        
     }
     public void Logout()
     {
