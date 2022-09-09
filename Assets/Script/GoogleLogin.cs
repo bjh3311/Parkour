b221d7adbf8f311efefdee67db2f59416d98112d
bjh3311 <bjh3311@naver.com>
2022-09-09 18:39:32 +0900

    진짜진짜 끝

--- Assets/Script/GoogleLogin.cs
+++ Assets/Script/GoogleLogin.cs
@@ -4,15 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SocialPlatforms;
+using UnityEngine.SceneManagement;
 
 using GooglePlayGames;
 using GooglePlayGames.BasicApi;
 
 
 public class GoogleLogin : MonoBehaviour
-{
-
-    
+{   
     public Text temp;
     void Start()
     {
@@ -23,10 +22,28 @@ public class GoogleLogin : MonoBehaviour
     }
     public void Login()//구글플레이 로그인 버튼에 적용
     {
-        
+        Social.localUser.Authenticate
+        (
+            (bool success) =>
+            {    
+                if(success)//성공시에
+                {
+                    StartCoroutine("LoadMain");
+                    string userInfo="Username: " + Social.localUser.userName +
+                    "\nUser ID: " + Social.localUser.id +
+                    "\nIsUnderage: " + Social.localUser.underage;
+                    temp.text=userInfo;
+                }
+                else
+                {
+                    temp.text="실패";
+                }
+            }
+        );
     }
-    public void Logout()
+     IEnumerator LoadMain()//구글플레이 로그인 성공하고 4초 이따가 Main화면 불러들임
     {
-        
+        yield return new WaitForSecondsRealtime(4.0f);
+        SceneManager.LoadScene("Main");
     }
 }
