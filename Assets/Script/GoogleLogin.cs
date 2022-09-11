0f449196fc80b7c72c087ecfd3f8675d9f7d017b
bjh3311 <bjh3311@naver.com>
2022-09-11 17:19:49 +0900

    안되면 android setup이나 force resolve 한번씩 하기

--- Assets/Script/GoogleLogin.cs
+++ Assets/Script/GoogleLogin.cs
@@ -15,13 +15,12 @@ public class GoogleLogin : MonoBehaviour
     public Text temp;
     void Start()
     {
-        
         PlayGamesPlatform.DebugLogEnabled=true;   
-        
+        PlayGamesPlatform.Activate();//구글플레이 플랫폼 활성화
     }
     public void Login()//구글플레이 로그인 버튼에 적용
     {
-        PlayGamesPlatform.Activate();//구글플레이 플랫폼 활성화
+        
         //위의 함수를 실행하면 Social.Active= PlayGamesPlatform.Instance가 된다
         Social.localUser.Authenticate
         (
